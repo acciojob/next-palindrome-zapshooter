63abbc35ae2c1ffcf844c7790b81501b68018627
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-04-08 22:30:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function nextPalindrome(num) {
-  //your JS code here.
+  num = Number(num);
+  let candidate = num + 1;
+
+  while (!isPalindrome(candidate)) {
+    candidate++;
+  }
+
+  return candidate;
 }
 
-const input = prompt("Enter a palindrome number");
+function isPalindrome(n) {
+  const s = String(n);
+  return s === s.split('').reverse().join('');
+}
+
+const input = prompt("Enter a number");
 alert(nextPalindrome(input));
